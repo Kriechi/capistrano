7272c7766eba700c8b7952f018297f205bb2488a
Lucas Jenss <lucas@x3ro.de>
2014-05-07 16:36:03 +0200

    Refactor enumeration of deployed releases
    As mentioned in #994, our project structure requires folders other
    than actual releases to exist in the releases_path. To be able to
    customize the logic to enumerate releases (to exclude said non-
    release folders), it has been refactored into a "configuration lambda"
    :releases.

--- lib/capistrano/deploy.rb
+++ lib/capistrano/deploy.rb
@@ -1,4 +1,17 @@
 require 'capistrano/framework'
 require 'capistrano-stats'
 
+# Fetches the currently deployed releases. Can be overwritten by setting :releases to a
+# different lambda.
+#
+# Example (inside a task)
+#
+#   releases = fetch(:releases).call(self)
+#   # => ["20140507122109", "20140507122237", "20140507122531"]
+#
+# Returns an array of folder names inside the releases_path, sorted oldest first.
+set :releases, lambda { |context|
+  context.capture(:ls, "-xt #{context.releases_path}").split.sort
+}
+
 load File.expand_path("../tasks/deploy.rake", __FILE__)
